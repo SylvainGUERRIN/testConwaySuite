8b45d25652ada25d570f48caca58a4a53264e6eb
SylvainG <sguerrin549@gmail.com>
2020-07-27 18:55:20 +0200

    Conway suite add multiple lines

--- tests/ConwaySuite.php
+++ tests/ConwaySuite.php
@@ -10,7 +10,7 @@ class ConwaySuite
     private $spaceSeparator = " ";
     private $emptyString = "";
 
-    public function draw($line): string
+    public function draw($line, $deep): string
     {
         return $this->drawSourceLine($line) . $this->drawNextLine($line);
     }
@@ -31,13 +31,46 @@ class ConwaySuite
 
     public function drawLineChunks($compressedLine, $chunks): string
     {
-        if(strlen($compressedLine) > 1 && $compressedLine[0] !== $compressedLine[1]){
-            return $this->drawLineChunks(substr($compressedLine, 1),
-                $chunks . $this->countConsecutiveLineNumbers(substr($compressedLine, 0, 1)) . $this->spaceSeparator);
+        if(empty($compressedLine)){
+            return trim($chunks);
         }
-        return $chunks . $this->countConsecutiveLineNumbers($compressedLine);
+        $indexOfNextDistinctNumber = $this->indexOfNextDistinctNumber($compressedLine, 0);
+//        if(strlen($compressedLine) > 1 && $compressedLine[0] !== $compressedLine[1]){
+            return $this->drawLineChunks(
+                $this->tail($compressedLine, $indexOfNextDistinctNumber + 1),
+                    $this->accumulateChunk($compressedLine, $chunks, $indexOfNextDistinctNumber) . $this->spaceSeparator);
+//        }
+//        return $chunks . $this->countConsecutiveLineNumbers($compressedLine);
         //return $this->countConsecutiveLineNumbers(substr($compressedLine, 0, 1))
             //. $this->spaceSeparator . $this->countConsecutiveLineNumbers(substr($compressedLine, 1));
+    }
+
+    public function accumulateChunk($compressedLine, $chunks, $indexOfNextDistinctNumber): String
+    {
+        return $chunks . $this->countConsecutiveLineNumbers(substr($compressedLine, 0, $indexOfNextDistinctNumber + 1));
+    }
+
+    public function tail($compressedLine, $i): String
+    {
+        return substr($compressedLine, $i);
+    }
+
+    public function indexOfNextDistinctNumber($compressedLine, $index)
+    {
+        if($this->hasOnlyOneNumber($compressedLine)|| $this->isNextNumberDistinct($compressedLine)){
+            return $index;
+        }
+        return $this->indexOfNextDistinctNumber($this->tail($compressedLine, 1), $index + 1);
+    }
+
+    public function isNextNumberDistinct($compressedLine): bool
+    {
+        return $compressedLine[0] !== $compressedLine[1];
+    }
+
+    public function hasOnlyOneNumber($compressedLine): bool
+    {
+        return strlen($compressedLine) === 1;
     }
 
     public function removeLineSpaces($line): string
--- tests/ConwayTest.php
+++ tests/ConwayTest.php
@@ -11,13 +11,14 @@ class ConwayTest extends TestCase
 {
     public function testShouldDrawNextLineOfConwaySuite(): void
     {
-        $this->expectConwaySuite("1", "1\n1 1");
-        $this->expectConwaySuite("2", "2\n1 2");
-        $this->expectConwaySuite("2 2", "2 2\n2 2");
-        $this->expectConwaySuite("2 1", "2 1\n1 2 1 1");
-        $this->expectConwaySuite("2 1 3", "2 1 3\n1 2 1 1 1 3");
-        $this->expectConwaySuite("2 1 1 1", "2 1 1 1\n1 2 3 1");
-        $this->expectConwaySuite("2 1 1 1", "2 1 1 1\n1 2 3 1");
+        $this->expectConwaySuite("1", 1, "1\n1 1");
+        $this->expectConwaySuite("2", 1, "2\n1 2");
+        $this->expectConwaySuite("2 2", 1, "2 2\n2 2");
+        $this->expectConwaySuite("2 1", 1, "2 1\n1 2 1 1");
+        $this->expectConwaySuite("2 1 3", 1, "2 1 3\n1 2 1 1 1 3");
+        $this->expectConwaySuite("2 1 1 1", 1, "2 1 1 1\n1 2 3 1");
+        $this->expectConwaySuite("2 1 1 1", 1, "2 1 1 1\n1 2 3 1");
+        $this->expectConwaySuite("2 3 3 1", 1, "2 3 3 1\n1 2 2 3 1 1");
         //$conwaySuite = new ConwaySuite();
         //self::assertThat($conwaySuite->draw("1"), self::equalTo("1\n1 1"));
         //self::assertThat($conwaySuite->draw("2"), self::equalTo("2\n1 2"));
@@ -26,9 +27,15 @@ class ConwayTest extends TestCase
         //self::assertThat($conwaySuite->draw("2 1 3"), self::equalTo("2 1 3\n1 2 1 1 1 3"));
     }
 
-    private function expectConwaySuite($line, $expectedLines): void
+    public function testShouldDrawSeveralNextLinesOfConwaySuite(): void
+    {
+        $this->expectConwaySuite("1", 2,  "1 \n1 1\n2 1");
+
+    }
+
+    private function expectConwaySuite($line, $deep, $expectedLines): void
     {
         $conwaySuite = new ConwaySuite();
-        self::assertThat($conwaySuite->draw($line), self::equalTo($expectedLines));
+        self::assertThat($conwaySuite->draw($line, $deep), self::equalTo($expectedLines));
     }
 }
